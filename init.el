5e806790bfb31e4d411da0630f25fae62c3d0198
Doug Beney <contact@dougie.io>
2019-05-01 17:09:27 -0400

    monokai

--- init.el
+++ init.el
@@ -70,8 +70,8 @@
 ;;;
 
 ;;(use-package atom-one-dark-theme)
-(use-package dracula-theme)
-;;(use-package monokai-theme)
+;;(use-package dracula-theme)
+(use-package monokai-theme)
 ;; (use-package doom-themes)
 
 ;;;
